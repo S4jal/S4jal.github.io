7e4a1f4f51ea914e342868c141855a58b671193e
Mehedi H Sajal <sajal.voice@gmail.com>
2026-02-01 11:15:26 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const cards = document.querySelectorAll('.tilt');
+
+cards.forEach(card => {
+    card.addEventListener('mousemove', (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateX = ((y - centerY) / centerY) * 10;
+        const rotateY = ((x - centerX) / centerX) * 10;
+
+        card.style.transform = `
+            rotateX(${-rotateX}deg)
+            rotateY(${rotateY}deg)
+            scale(1.05)
+        `;
+        card.style.boxShadow = "0 20px 40px rgba(99,102,241,0.3)";
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = "rotateX(0) rotateY(0) scale(1)";
+        card.style.boxShadow = "none";
+    });
+});
